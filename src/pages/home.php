4e190089a63cd9d089f9a5aa992032a0a802bc34
Daniel Segatto <dnsegatto1@gmail.com>
2025-05-07 14:44:58 -0300

    Atualizado

--- src/pages/home.php
+++ src/pages/home.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="../../styles/css/styles.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+	
 	<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 	<title>Portifólio</title>
 </head>
